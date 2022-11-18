8547b1de22b38c6b71a41c7d4f12a7f594d02559
Juliet Onyekaoha <juliet@bento.africa>
2022-11-18 08:20:44 +0100

    initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  singleQuote: true,
+  semi: false,
+  overrides: [
+    {
+      files: ['**/*.js'],
+      options: {
+        singleQuote: true,
+      },
+    },
+  ],
+}
---
+++ src/views/EventList.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Event Listing</h1>
+    <event-card />
+    <base-icon />
+  </div>
+</template>
+
+<script>
+import EventCard from '@/components/EventCard.vue'
+import BaseIcon from '@/components/BaseIcon.vue'
+
+export default {
+  components: { EventCard, BaseIcon },
+}
+</script>
